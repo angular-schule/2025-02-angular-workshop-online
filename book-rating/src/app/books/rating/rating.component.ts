51325e130513be132cc9aaabdddc5f253ba7d4c8
Ferdinand Malcher <ferdinand@malcher.media>
2025-02-20 08:18:33 +0100

    Hausaufgabe: Sterne

---
+++ book-rating/src/app/books/rating/rating.component.ts
@@ -0,0 +1,13 @@
+import { Component, computed, input } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  imports: [],
+  templateUrl: './rating.component.html',
+  styleUrl: './rating.component.scss'
+})
+export class RatingComponent {
+  readonly value = input.required<number>();
+
+  readonly ratingArray = computed(() => new Array(Math.max(this.value() || 0)));
+}
