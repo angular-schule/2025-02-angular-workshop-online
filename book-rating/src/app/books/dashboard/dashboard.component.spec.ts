4bf2382c27e5772e3c4cfecbb649de3bd77ca5dc
Ferdinand Malcher <ferdinand@malcher.media>
2025-02-20 14:06:49 +0100

    Test Dashboard: Provider für Rating-Mock

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,19 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
+
+    const ratingMock = {
+      rateUp: (b: Book) => b,
+      rateDown: (b: Book) => b,
+    };
+
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [DashboardComponent],
+      providers: [
+        // BRS ersetzen: Wenn jemand BookRatingService anfordert …
+        // … wird stattdessen ratingMock ausgeliefert
+        { provide: BookRatingService, useValue: ratingMock }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
+    // TS-Klasseninstanz
     component = fixture.componentInstance;
+
+    // DOM
+    // fixture.nativeElement
+
     fixture.detectChanges();
   });
 
