d2b976de6d99c6e14a172ef0079e043872ba9e3d
Ferdinand Malcher <ferdinand@malcher.media>
2025-02-21 11:24:24 +0100

    Routing: Routen und Outlet

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from './books/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DashboardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   readonly title = 'Book Rating';
 }
+
+/*
+TODO:
+- Redirect von Startseite ✅
+- Wildcard-Route ✅
+- Links
+  - BookComponent => Detailseite
+  - Detailseite => Dashboard
+- Detailseite
+  - Parameter aus der URL
+  - …
+
+*/
--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { booksRoutes } from './books/books.routes';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // bei Weiterleitung vom leeren Pfad (fast) immer pathMatch:full notwendig
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  ...booksRoutes,
+  { path: '**', component: ErrorComponent }
+];
